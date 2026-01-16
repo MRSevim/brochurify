320ed80409f2189e24ffa24d32cc9c97cf482e21
MRSevim <lionrasit@gmail.com>
2026-01-17 00:15:56 +0300

    added w-full to container and removed extra div in contact

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -10,14 +10,20 @@ const Container = ({
   if (isMain) {
     return (
       <main
-        className={"mx-auto px-4 my-8 max-w-7xl flex-1 " + `${className ?? ""}`}
+        className={
+          "mx-auto px-4 my-8 max-w-7xl flex-1 w-full " + `${className ?? ""}`
+        }
       >
         {children}
       </main>
     );
   } else {
     return (
-      <div className={"mx-auto flex-1 px-4 max-w-7xl " + `${className ?? ""}`}>
+      <div
+        className={
+          "mx-auto flex-1 px-4 max-w-7xl w-full " + `${className ?? ""}`
+        }
+      >
         {children}
       </div>
     );
