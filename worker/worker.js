ad03b2b78e187590b3144c0f251a8f9c742a4376
MRSevim <lionrasit@gmail.com>
2026-02-03 17:22:08 +0300

    added cleanup to restart worker

--- worker/worker.js
+++ worker/worker.js
@@ -10,7 +10,7 @@ const connection = new Redis({
   password: process.env.REDIS_PASSWORD,
   maxRetriesPerRequest: null,
 });
-console.log("trigger ci")
+
 const TABLE_NAME = process.env.DB_TABLE_NAME;
 
 const snapshotWorker = new Worker(
